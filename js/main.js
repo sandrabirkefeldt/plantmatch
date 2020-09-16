67ac14105ddceff8ff9191677af00544a1fa748f
sandrabirkefeldt <56107469+sandrabirkefeldt@users.noreply.github.com>
2020-09-16 10:06:15 +0200

    Version 8

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 "use strict";
+
+// SWIPE SIDEN
 import Carousel from "./carousel.js";
 
 let board = document.querySelector("#board");
+
 let carousel = new Carousel(board);
 
 let _plants = [];
@@ -17,42 +20,31 @@ async function getPlants() {
 getPlants();
 
 function appendPlants(plants) {
-  let template = "";
   for (const plant of plants) {
-    template += /*html*/ `
+    let template = /*html*/ `
     <article>
       <h2>${plant.title.rendered}</h2>
-      <div class="slideshow-container">
-        <img src="${plant.acf.image_1.url}">
-        <!-- <img src = "${plant.acf.image_2.url}" style = "width=100%">
-        <img src = "${plant.acf.image_3.url}" style = "width=100%" > -->
-  </div>
+      <img src="${plant.acf.image_1.url}">
+  </article>
+  <article>
+    <button class="knap-dislike">dislike</button>
+    <button class="knap-like">like</button>
+  
   </article>
 
-  <section>
-    <a href="#"><img class="like-dislike-knapper" src="img/like.png" alt="Like knap" style="width:42px;height:42px;"></a>
-        <a href="#"><img class="like-dislike-knapper" src="img/dislike.png" alt="Dislike knap" style="width:42px;height:42px;"></a>
-  </section>
+  
     `;
-    carousel.push(template);
+    carousel.push(template, plant.id);
   }
   carousel.handle();
 }
 
-// append plants to the DOM
+window.like = function like(id) {
+  console.log("like, post id " + id);
+}
 
-// function appendPlants(plants) {
-//   let htmlTemplate = "";
-//   for (let plant of plants) {
-//     htmlTemplate += /*html*/ `
-//       <article>
-//         <h2>${plant.title.rendered}</h2>
-//         <p>Størrelse: ${plant.acf.storrelse}</p>
-//         <p>Familie: ${plant.acf.familie}</p>
-//         <p>Oprindelse: ${plant.acf.oprindelse}</p>
-//         <p>${plant.content.rendered}</p>
-//       </article>
-//     `;
-//   }
-//   
-//}
+window.dislike = function dislike(id) {
+  console.log("dislike, post id " + id);
+}
+
+// FAVORIT SIDEN
