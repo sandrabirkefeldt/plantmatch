05c9b396c04cbfa806bdc652f4fe6e99f7acf1df
sofi062d <56107487+sofi062d@users.noreply.github.com>
2020-09-17 11:45:59 +0200

    Version 13

--- js/main.js
+++ js/main.js
@@ -82,3 +82,87 @@ function showFavorit(title) {
 }
 
 // Oplysningsside for hver plante - Her kan man læse om den enkelte liket plante
+
+
+
+
+
+
+
+// Web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDqo3bvIq7-YaxChuVF4GShgpuaffy8T1M",
+  authDomain: "plant-match-bef7b.firebaseapp.com",
+  databaseURL: "https://plant-match-bef7b.firebaseio.com",
+  projectId: "plant-match-bef7b",
+  storageBucket: "plant-match-bef7b.appspot.com",
+  messagingSenderId: "46789833113",
+  appId: "1:46789833113:web:87ee664e9519c0e38b6308"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+let _firebaseUI;
+
+// ========== FIREBASE AUTH ========== //
+// Listen on authentication state change
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) { // if user exists and is authenticated
+    userAuthenticated(user);
+  } else { // if user is not logged in
+    userNotAuthenticated();
+  }
+});
+
+function userAuthenticated(user) {
+  appendUserData(user);
+  hideTabbar(false);
+  showLoader(false);
+}
+
+function userNotAuthenticated() {
+  hideTabbar(true);
+  showPage("login");
+
+  // Firebase UI configuration
+  const uiConfig = {
+    credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+    signInOptions: [
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID
+    ],
+    signInSuccessUrl: '#home'
+  };
+  // Init Firebase UI Authentication
+  if (!_firebaseUI) {
+    _firebaseUI = new firebaseui.auth.AuthUI(firebase.auth());
+  }
+  _firebaseUI.start('#firebaseui-auth-container', uiConfig);
+  showLoader(false);
+}
+
+// show and hide tabbar
+function hideTabbar(hide) {
+  let tabbar = document.querySelector('#tabbar');
+  if (hide) {
+    tabbar.classList.add("hide");
+  } else {
+    tabbar.classList.remove("hide");
+  }
+}
+
+
+// sign out user
+function logout() {
+  firebase.auth().signOut();
+}
+
+function appendUserData(user) {
+  console.log(user);
+  document.querySelector('#user-data').innerHTML = `
+    <img class="profile-img" src="${user.photoURL || "img/placeholder.jpg"}">
+    <h3>${user.displayName}</h3>
+    <p>${user.email}</p>
+  `;
+}
